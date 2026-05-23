d894fa587f2183dc56d5971a79205364cc3b0517
jackarry-hub <2499324419@qq.com>
2026-05-23 20:58:13 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/cyber-dad-ai-academy/",
   plugins: [react()],
   server: {
     host: "0.0.0.0",
